981d41921d090622468cd65bb399d7e6465cccbb
eva <130740071+eva-razzini@users.noreply.github.com>
2024-11-22 04:02:48 +0100

    fix model joueur after test

--- football/equipe/serializers.py
+++ football/equipe/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Equipe
+from .models import Equipe, Joueur
 
 
 class EquipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipe
         fields = ['id', 'nom', 'ville']
+
+
+class JoueurSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Joueur
+        fields = ['id', 'nom', 'poste', 'equipes']
